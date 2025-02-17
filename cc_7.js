c8747c8b301da104d61b457346a7d2c304f715d8
Jonas <jonashooncantey@gmail.com>
2025-02-16 19:16:47 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -16,3 +16,25 @@ function calculateHourlyWages(salary, hoursPerWeek) { //Created function that ta
 
 console.log(`Hourly Wage: $${calculateHourlyWages(52000,40)}`)
 console.log(`Hourly Wage: $${calculateHourlyWages(75000,35)}`)
+
+//Task 3
+console.log("Task 3")
+
+const calculateLoyaltyDiscount = (amount, years) => {   //Created arrow function here
+    let discount;       //defined a discount variable that changes based on years
+
+    if (years >= 5) {   //started by saying if 5 years or more than apply 15% discount
+        discount = 0.15;
+    } else if (years >= 3) {    //then went to say if 3 years or more than apply 10% discount
+        discount = 0.1
+    } else {                    //Finally said if any other argument, apply 5% discount
+        discount = 0.05
+    }
+    
+    const discountedPrice = amount * (1 - discount) //made the function that discounts the price
+
+    return `Discounted Price: $${discountedPrice.toFixed(2)}`;  //told the function what to output
+};
+
+console.log(calculateLoyaltyDiscount(100, 6));
+console.log(calculateLoyaltyDiscount(200, 2));
